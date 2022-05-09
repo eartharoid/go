d653b5f273fa9785880926bcaa592523f5de3dc9
Isaac <git@eartharoid.me>
2022-05-09 21:08:52 +0100

    Reset

--- globals.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	regex: {
-		url: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/gmi,
-		collectIP: /(^ip,)|(^ip$)|(,ip,)|(,ip$)/gi,
-	}
-};
